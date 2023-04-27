de4e48f84bd9223527b480eb8a19758311521d8c
FeichtingerLeona <116262109+FeichtingerLeona@users.noreply.github.com>
2023-04-27 11:54:33 +0200

    Add files via upload

---
+++ 053-TheMysteriousCaveOfNumericus/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+var rand = new Random(4711);
+var randomValue = new int[75000];
+decimal average = 0; ;
+for (int i = 0; i < randomValue.Length; i++)
+{
+    randomValue[i] = rand.Next(1, 1000000001);
+}
+bool doneChanges = false;
+do
+{
+    doneChanges = false;
+    for (int i = 0; i < (randomValue.Length - 1); i++)
+    {
+        int j = i + 1;
+        if (randomValue[i] > randomValue[j])
+        {
+            (randomValue[i], randomValue[j]) = (randomValue[j], randomValue[i]);
+            doneChanges = true;
+        }
+
+    }
+}
+while (doneChanges);
+
+for (int i = 0; i < (randomValue.Length - 1); i++)
+{
+    int j = i + 1;
+    average += (randomValue[j] - randomValue[i]);
+}
+average /= (randomValue.Length-1);
+System.Console.WriteLine(average);
