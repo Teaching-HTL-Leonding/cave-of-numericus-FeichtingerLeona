08f0125337f09ee6d8536a0d4ee30fddec1f43eb
FeichtingerLeona <116262109+FeichtingerLeona@users.noreply.github.com>
2023-04-27 12:37:09 +0200

    Add files via upload

---
+++ 053-TheMysteriousCaveOfNumericus2/Program.cs
@@ -0,0 +1,20 @@
+﻿decimal sumOfRandomNumbers = 1000000;
+var rand = new Random(4711);
+long randomValue = rand.Next(1, 1000000001);
+decimal min = randomValue;
+decimal max = randomValue;
+decimal average = 0;
+for (long i = 0; i < sumOfRandomNumbers; i++)
+{
+    randomValue = rand.Next(1, 1000000001);
+    if (min > randomValue)
+    {
+        min = randomValue;
+    }
+    else if (max < randomValue)
+    {
+        max = randomValue;
+    }
+}
+average = (max - min) / (sumOfRandomNumbers - 1);
+System.Console.WriteLine(average);
